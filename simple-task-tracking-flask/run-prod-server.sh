0a2423fa7894222a14e29475da8f2e29c1bf390d
Xue Xiao <xue-xiao@users.noreply.github.com>
2020-11-26 02:36:05 -0800

    Update startup scripts

--- simple-task-tracking-flask/run-prod-server.sh
+++ simple-task-tracking-flask/run-prod-server.sh
@@ -3,4 +3,4 @@
 # Need to add NGINX or ALB to the front of 5000 so that request is buffered
 # Configure ./instance/prod_config.py with the correct database URL.
 
-gunicorn -w 2 -k gevent -b 0.0.0.0:5000 "app:create_app(stage='prod')"
+gunicorn -w 2 -k gevent -b localhost:5000 "app:create_app(stage='prod')"
